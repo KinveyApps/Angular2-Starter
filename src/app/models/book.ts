c1139462b6aa16392e6c9955bdc3977431fd4dad
Thomas Conner <thomas.conner@me.com>
2017-12-13 13:52:46 -0500

    Fix models

--- src/app/models/book.ts
+++ src/app/models/book.ts
@@ -2,8 +2,6 @@ import { Kinvey } from 'kinvey-angular2-sdk';
 
 export class Book implements Kinvey.Entity {
   _id: string;
-  _acl: {};
-  _kmd: {};
   author: string;
   title: string;
   isbn: string;
--- src/app/models/file.ts
+++ src/app/models/file.ts
@@ -2,8 +2,6 @@ import { Kinvey } from 'kinvey-angular2-sdk';
 
 export class File implements Kinvey.File {
   _id: string;
-  _acl: {};
-  _kmd: {};
   _filename: string;
   _downloadURL: string;
   _expiresAt: string;
